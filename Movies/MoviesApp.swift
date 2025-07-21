bd7d997ccdbbb8236b15d753bad15ad98761db74
OmarIsmail2019 <OmarIsmail2019@github.com>
2025-07-21 10:47:49 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by MacBookPro on 21/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
